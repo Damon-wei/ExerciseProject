b665b5720be3b8a28f14ff12c19df3f64c3afe50
Damon-wei <18612124664@163.com>
2017-03-17 17:28:42 +0800

    运行时权限练习

--- litepaltest/src/main/java/com/example/litepaltest/MainActivity.java
+++ litepaltest/src/main/java/com/example/litepaltest/MainActivity.java
@@ -1,18 +1,24 @@
 package com.example.litepaltest;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import org.litepal.crud.DataSupport;
-import org.litepal.tablemanager.Connector;
 
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+
+import static android.os.Build.VERSION_CODES.M;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
     Button btnUpdate;
     @BindView(R.id.btn_query)
     Button btnQuery;
+    @BindView(R.id.call)
+    Button call;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         btnCreate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                DataSupport.deleteAll(Book.class,"price < ?","99");
+                DataSupport.deleteAll(Book.class, "price < ?", "99");
             }
         });
         btnAdd.setOnClickListener(new View.OnClickListener() {
@@ -53,19 +61,59 @@ public class MainActivity extends AppCompatActivity {
                 Book book = new Book();
                 book.setPages(9);
                 book.setPrice(9.9);
-                int code = book.updateAll("name = ? and author = ?","abc","damon");
-                L.d("code=="+code);
+                int code = book.updateAll("name = ? and author = ?", "abc", "damon");
+                L.d("code==" + code);
             }
         });
         btnQuery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //List<Book> books = DataSupport.findAll(Book.class);
-                List<Book> books = DataSupport.select("name","author").find(Book.class);
-                for (Book book:books) {
+                List<Book> books = DataSupport.select("name", "author").find(Book.class);
+                for (Book book : books) {
                     L.d(book.toString());
                 }
             }
         });
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CALL_PHONE},1);
+                }
+                call();
+            }
+        });
+    }
+
+    private void call() {
+        try{
+            Intent intent = new Intent(Intent.ACTION_CALL);
+            intent.setData(Uri.parse("tel:10086"));
+            startActivity(intent);
+        }catch (SecurityException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        T.showShort(MainActivity.this,"请开启打电话权限");
+        switch (requestCode){
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    call();
+                }else {
+                    T.showShort(MainActivity.this,"请开启打电话权限");
+                }
+                break;
+        }
     }
 }
---
+++ litepaltest/src/main/java/com/example/litepaltest/T.java
@@ -0,0 +1,18 @@
+package com.example.litepaltest;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by Damon on 2017/3/17.
+ * Description :
+ */
+
+public class T {
+    public T() {
+
+    }
+    public static void showShort(Context context,String msg){
+        Toast.makeText(context,msg,Toast.LENGTH_SHORT).show();
+    }
+}
