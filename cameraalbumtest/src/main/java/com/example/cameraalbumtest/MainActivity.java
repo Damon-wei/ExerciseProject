5b31c0adf4185e593c449ecb3e67b713d024d7df
Damon-wei <18612124664@163.com>
2017-03-24 17:24:48 +0800

    拍照，约束布局练习

---
+++ cameraalbumtest/src/main/java/com/example/cameraalbumtest/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.cameraalbumtest;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnTakePhoto;
+    private ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnTakePhoto = (Button) findViewById(R.id.btn);
+        imageView = (ImageView) findViewById(R.id.img);
+
+        btnTakePhoto.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+        }else {
+            takePhoto();
+        }
+
+    }
+
+    private void takePhoto() {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    takePhoto();
+                }
+                break;
+            default:
+        }
+    }
+}
