d6b3e92e2497941bf0fcd28624de1527aab52e3d
Damon-wei <18612124664@163.com>
2017-03-27 16:25:41 +0800

    从相册选取图片，添加图片压缩逻辑

--- cameraalbumtest/src/main/java/com/example/cameraalbumtest/MainActivity.java
+++ cameraalbumtest/src/main/java/com/example/cameraalbumtest/MainActivity.java
@@ -1,19 +1,25 @@
 package com.example.cameraalbumtest;
 
 import android.Manifest;
+import android.content.ContentUris;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Build;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -21,11 +27,14 @@ import android.widget.ImageView;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.math.BigDecimal;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     public static final int TAKE_PHOTO = 1;
+    public static final int CHOOSE_PHOTO = 2;
     private Button btnTakePhoto;
+    private Button btnChoosePhoto;
     private ImageView imageView;
     private Uri imageUri;
     @Override
@@ -33,20 +42,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnTakePhoto = (Button) findViewById(R.id.btn);
+        btnChoosePhoto = (Button) findViewById(R.id.btn_choose_from_album);
         imageView = (ImageView) findViewById(R.id.img);
 
         btnTakePhoto.setOnClickListener(this);
+        btnChoosePhoto.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
-        }else {
-            takePhoto();
-
+        switch (view.getId()){
+            case R.id.btn:
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},TAKE_PHOTO);
+                }else {
+                    takePhoto();
+                }
+                break;
+            case R.id.btn_choose_from_album:
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},CHOOSE_PHOTO);
+                }else {
+                    choosePhoto();
+                }
+                break;
         }
 
+
+    }
+
+    private void choosePhoto() {
+        Intent intent = new Intent("android.intent.action.GET_CONTENT");
+        intent.setType("image/*");
+        startActivityForResult(intent,CHOOSE_PHOTO);
     }
 
     private void takePhoto() {
@@ -78,12 +106,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (requestCode){
             case TAKE_PHOTO:
                 if (resultCode == RESULT_OK) {
-                    try {
-                        Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
-                        imageView.setImageBitmap(bitmap);
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                    }
+                    Bitmap bitmap = compressBitmap(imageUri.getPath());
+                    Log.d("damon","path=="+imageUri.getPath());
+                    //Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                    imageView.setImageBitmap(bitmap);
+                }
+                break;
+            case CHOOSE_PHOTO:
+                if (Build.VERSION.SDK_INT >= 19) {
+                    //安卓4.4及以上版本选取相册中的图片之后不再直接返回uri，需要解析
+                    handleImageOnKitkat(data);
+                }else {
+                    handleImageBeforeKitkat(data);
                 }
                 break;
             default:
@@ -91,15 +125,100 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    private void handleImageBeforeKitkat(Intent data) {
+        Uri uri = data.getData();
+        String imagePath = getImagePath(uri,"");
+        displayImage(imagePath);
+    }
+
+    private void displayImage(String imagePath) {
+        if (imagePath != null) {
+            Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+            imageView.setImageBitmap(bitmap);
+        }
+    }
+
+    private String getImagePath(Uri uri, String selection) {
+        String path = null;
+        //通过uri和selection来获取真实的图片路径
+        Cursor cursor = getContentResolver().query(uri,null,selection,null,null);
+        if (cursor != null) {
+            if (cursor.moveToNext()) {
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+            cursor.close();
+        }
+        return path;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void handleImageOnKitkat(Intent data) {
+        String imagePath = null;
+        Uri uri = data.getData();
+        if (DocumentsContract.isDocumentUri(this,uri)) {
+            //如果是 Document 类型的 uri，则通过 document id 处理
+            String docId = DocumentsContract.getDocumentId(uri);
+            if ("com.android.providers.media.documents".equals(uri.getAuthority())) {
+                String id = docId.split(":")[1];//解析出数字格式的id
+                String selection = MediaStore.Images.Media._ID + "=" + id;
+                imagePath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,selection);
+            }else if ("com.android.providers.downloads.documents".equals(uri.getAuthority())){
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"),Long.valueOf(docId));
+                imagePath = getImagePath(contentUri,null);
+            }
+        }else if ("content".equalsIgnoreCase(uri.getScheme())){
+            //如果是content类型的Uri, 则直接使用普通的方式处理
+            imagePath = getImagePath(uri,null);
+        }else if ("file".equalsIgnoreCase(uri.getScheme())){
+            //如果是fiel类型的Uri, 直接获取图片路径即可
+            imagePath = uri.getPath();
+        }
+        displayImage(imagePath);
+    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode){
-            case 1:
+            case TAKE_PHOTO:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    choosePhoto();
+                }
+                break;
+            case CHOOSE_PHOTO:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     takePhoto();
                 }
                 break;
             default:
+        }
+    }
+    /**
+     * 压缩图片
+     * @return Bitmap
+     */
+    public static Bitmap compressBitmap(String path) {
+        // 如果想要的宽度和高度都比源图片小，就不压缩了，直接返回原图
+        int width = 500;
+        int height = 500;
+        boolean isAdjust = true;
+        Bitmap bitmap = BitmapFactory.decodeFile(path);
+
+        if (bitmap.getByteCount() / 1024 < 1000) {
+            return bitmap;
+        } else {
+            if (bitmap.getWidth() < width && bitmap.getHeight() < height) {
+                return bitmap;
+            }
+            // 根据想要的尺寸精确计算压缩比例, 方法详解：public BigDecimal divide(BigDecimal divisor, int scale, int roundingMode);
+            // scale表示要保留的小数位, roundingMode表示如何处理多余的小数位，BigDecimal.ROUND_DOWN表示自动舍弃
+            float sx = new BigDecimal(width).divide(new BigDecimal(bitmap.getWidth()), 4, BigDecimal.ROUND_DOWN).floatValue();
+            float sy = new BigDecimal(height).divide(new BigDecimal(bitmap.getHeight()), 4, BigDecimal.ROUND_DOWN).floatValue();
+            if (isAdjust) {// 如果想自动调整比例，不至于图片会拉伸
+                sx = (sx < sy ? sx : sy);sy = sx;// 哪个比例小一点，就用哪个比例
+            }
+            Matrix matrix = new Matrix();
+            matrix.postScale(sx, sy);// 调用api中的方法进行压缩，就大功告成了
+            return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
         }
     }
 }
