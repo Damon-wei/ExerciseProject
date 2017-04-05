601256604145efc7acb6d545557b5821a2fa8868
Damon-wei <18612124664@163.com>
2017-04-05 11:22:46 +0800

    LBSTest

--- lbstest/src/main/java/com/example/lbstest/MainActivity.java
+++ lbstest/src/main/java/com/example/lbstest/MainActivity.java
@@ -14,22 +14,37 @@ import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    //version 1.0,修复bug
     private LocationClient mLocationClient;
     private TextView tvLocation;
+    private MapView mapView;
+    private BaiduMap baiduMap;
+    private boolean isFirstLcation = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
         mLocationClient = new LocationClient(getApplicationContext());
         mLocationClient.registerLocationListener(new MyLocationListener());
         setContentView(R.layout.activity_main);
 
         tvLocation = (TextView) findViewById(R.id.tv_location);
+        mapView = (MapView) findViewById(R.id.map);
+        baiduMap = mapView.getMap();
+        baiduMap.setMyLocationEnabled(true);
 
         List<String> permissionList = new ArrayList<>();
         if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -49,12 +64,30 @@ public class MainActivity extends AppCompatActivity {
         }
 
         initLocation();
+
     }
 
+    private void navigateTo(BDLocation location){
+        if (isFirstLcation) {
+            LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
+            MapStatusUpdate update = MapStatusUpdateFactory.newLatLng(latLng);
+            baiduMap.animateMapStatus(update);
+            update = MapStatusUpdateFactory.zoomTo(16f);
+            baiduMap.animateMapStatus(update);
+            isFirstLcation = false;
+        }
+        MyLocationData.Builder builder = new MyLocationData.Builder();
+        builder.latitude(location.getLatitude());
+        builder.longitude(location.getLongitude());
+        MyLocationData locationData = builder.build();
+        baiduMap.setMyLocationData(locationData);
+
+    }
     private void initLocation() {
         LocationClientOption option = new LocationClientOption();
         option.setScanSpan(3000);
         option.setIsNeedAddress(true);
+        option.setCoorType("bd09ll");
         mLocationClient.setLocOption(option);
         mLocationClient.start();
     }
@@ -63,7 +96,8 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onReceiveLocation(BDLocation bdLocation) {
-            final StringBuilder currentPosition = new StringBuilder();
+            navigateTo(bdLocation);
+            /*final StringBuilder currentPosition = new StringBuilder();
             currentPosition.append("维度：").append(bdLocation.getLatitude()).append("\n");
             currentPosition.append("经度：").append(bdLocation.getLongitude()).append("\n");
             currentPosition.append("位置：").append(bdLocation.getAddrStr()).append("\n");
@@ -78,7 +112,7 @@ public class MainActivity extends AppCompatActivity {
                 public void run() {
                     tvLocation.setText(currentPosition);
                 }
-            });
+            });*/
 
         }
         @Override
@@ -108,10 +142,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
     protected void onDestroy() {
         super.onDestroy();
         if (mLocationClient != null) {
             mLocationClient.stop();
         }
+        mapView.onDestroy();
     }
 }
